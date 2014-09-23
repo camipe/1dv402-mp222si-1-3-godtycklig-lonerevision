cd81e8cc55c9afece59a9821077e98b941936296
Micael Persson <micael.c.persson@gmail.com>
2014-09-23 13:50:25 +0200

    Created projectfiles and copied .gitignore from previous project repo.

---
+++ 1DV402.S1.L03A/1DV402.S1.L03A/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1DV402.S1.L03A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
