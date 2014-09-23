de6627dc864c524d698b8d5dbd8835d7820e134b
Micael Persson <micael.c.persson@gmail.com>
2014-09-23 15:46:23 +0200

    ProcessSalaries is now calculating average and spread. Removed previous change to ReadInt. Using String.Format where a placeholder is needed instead.

--- 1DV402.S1.L03A/1DV402.S1.L03A/Program.cs
+++ 1DV402.S1.L03A/1DV402.S1.L03A/Program.cs
@@ -10,27 +10,47 @@ namespace _1DV402.S1.L03A
     {
         static void Main(string[] args)
         {
-            int numberOfSalaries = ReadInt("Ange antal löner att mata in: ", 0);
+            int numberOfSalaries = ReadInt("Ange antal löner att mata in: ");
 
             ProcessSalaries(numberOfSalaries);
 
         }
 
-        static int ReadInt(string prompt, int var)
+        static int ReadInt(string prompt)
         {
-            Console.Write(prompt, var);
+            Console.Write(prompt,);
             int value = int.Parse(Console.ReadLine());
             return value;
         }
 
         static void ProcessSalaries(int count)
         {
+            // Declaring variables
             int[] salaries = new int[count];
+            int[] sortedSalaries = new int[count];
+            double averageSalary;            
+            int spreadSalary;
+            double medianSalary;
 
+
+            // Asking for the salaries and inserts them in the array salaries
             for (int i = 0; i < salaries.Length; i++)
             {
-                salaries[i] = ReadInt("Ange lön nummer {0}: ", i + 1);
-            }
+                salaries[i] = ReadInt(string.Format("Ange lön nummer {0}: ", i + 1));
+            }
+
+            // Adding all integers in salaries together
+            averageSalary = salaries.Sum();
+
+            // Calculating the spread between the largest and smallest salary
+            spreadSalary = salaries.Max() - salaries.Min();
+
+            // Creates a copy of salaries and sorts it in a min -> max order
+            sortedSalaries = (int[])salaries.Clone();
+            Array.Sort(sortedSalaries);
+
+
+
         }
 
     }
