aec880de27a3a3ed43903e6404bcd546e734db70
Micael Persson <micael.c.persson@gmail.com>
2014-09-23 21:33:44 +0200

    Finished with the parts that calculates median, average and spread.

--- 1DV402.S1.L03A/1DV402.S1.L03A/Program.cs
+++ 1DV402.S1.L03A/1DV402.S1.L03A/Program.cs
@@ -18,7 +18,7 @@ namespace _1DV402.S1.L03A
 
         static int ReadInt(string prompt)
         {
-            Console.Write(prompt,);
+            Console.Write(prompt);
             int value = int.Parse(Console.ReadLine());
             return value;
         }
@@ -31,7 +31,7 @@ namespace _1DV402.S1.L03A
             double averageSalary;            
             int spreadSalary;
             double medianSalary;
-
+            int middle;
 
             // Asking for the salaries and inserts them in the array salaries
             for (int i = 0; i < salaries.Length; i++)
@@ -39,8 +39,8 @@ namespace _1DV402.S1.L03A
                 salaries[i] = ReadInt(string.Format("Ange lön nummer {0}: ", i + 1));
             }
 
-            // Adding all integers in salaries together
-            averageSalary = salaries.Sum();
+            // Adding all integers in salaries together and dividing by number of salaries
+            averageSalary = Convert.ToDouble(salaries.Sum()) / salaries.Length;
 
             // Calculating the spread between the largest and smallest salary
             spreadSalary = salaries.Max() - salaries.Min();
@@ -48,9 +48,17 @@ namespace _1DV402.S1.L03A
             // Creates a copy of salaries and sorts it in a min -> max order
             sortedSalaries = (int[])salaries.Clone();
             Array.Sort(sortedSalaries);
-
-
-
+            
+            // Calculating the median value
+            middle = sortedSalaries.Length / 2; 
+            if ((sortedSalaries.Length % 2) != 0 )
+            {
+                medianSalary = sortedSalaries[sortedSalaries.Length / 2];
+            }
+            else
+            {
+                medianSalary = (sortedSalaries[middle] + sortedSalaries[middle - 1]) / 2d;
+            }            
         }
 
     }
